339c962ea750c0fdbcf1f33dd16d7fb71208d412
Erhan <erhantezer68@gmail.com>
2023-05-03 22:12:13 +0300

    new project started

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import { 
+    BrowserRouter, 
+    // Route, 
+    Routes 
+} from "react-router-dom"
+import Navbar from "../components/Navbar"
+
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                {/* <Route path="" element={} />
+                <Route path="" element={} />
+                <Route path="" element={} />
+                <Route path="" element={} />
+                <Route path="" element={} /> */}
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRouter
