f553bcdb29a22b85264a318bf86548bcd1474943
Erhan <erhantezer68@gmail.com>
2023-05-10 09:14:23 +0300

    updated hakkımızda

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,48 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div>
+        <h1>
+          Kurumsal Profil
+        </h1>
+        <p>
+          Firmamız 1996 yılından beri Bilişim Sektöründe hizmet vermektedir. "En yeni iletişim ve bilgisayar teknolojilerini kullanarak müşterilerine dünya standartlarında çözümler sunmak" ilkesiyle yola çıkan Berk Otomasyon, 1997 yılından bu yana Vega Yazılım çözüm ortağı olarak hizmet vermektedir. 
+        </p>
+      </div>
+      <div>
+        <h1>
+          Kalite Politikası
+        </h1>
+        <p>
+          Firmamız tüm süreçlerde, müşterinin ihtiyaç ve beklentilerini tam olarak karşılayan bir anlayış içinde güvenilir bir politika izlemeyi taahhüt ediyoruz. 
+        </p>
+      </div>
+      <div>
+        <h1>
+          Bilgi Güvenliği Politikası
+        </h1>
+        <p>
+          Bilgi güvenliğinin sağlanması Berk Software Solutions için hayati önem taşır. Firmamız bu amaçla sahip olduğu tüm bilgi varlıklarını korumayı bilgi güvenliği şartlarının uluslararası standartlar çerçevesinde sağlamayı taahhüt ediyoruz. 
+        </p>
+      </div>
+      <div>
+        <h1>
+          Yazılımsal Çözümler
+        </h1>
+        <p>
+          Firmamız, Vega Yazılım Firması ile müşterilerine yazılım çözümleri sunmaktadır. Ticari otomasyonlar, E-dönüşüm uygulamaları (e-fatura, e-arşiv, e-irsaliye, e-defter, e-mutabakat, e-imza), Mağazacılık Otomasyonları, Market Otomasyonları, Restoran Otomasyonları, Sıcak Satış Otomasyonları, Üretim Otomasyonları, Oto Satış-Oto Servis-Oto Yedek Parça Otomasyonları, Mobilya Üretim otomasyonları, İlaç Takip Sistemi Otomasyonları, Sanal Mağaza Otomasyonları ve buna benzer onlarca sektörde size destek sağlıyoruz. 
+        </p>
+      </div>
+      <div>
+        <h1>
+          Donanımsal Çözümler
+        </h1>
+        <p>
+          Firmamız güçlü kadrosu ile sizin işletmenizin istekleri ve ihtiyaçlarına yönelik donanım destekleri çözümler sunuyoruz. 
+        </p>
+      </div>
+    </div>
   )
 }
 
